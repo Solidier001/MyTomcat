550bea6431e7bbd49af5978fb30c79ad03b9b43a
世杰 孙 <2532592455@qq.com>
2022-10-24 20:16:02 +0800

    提交

--- src/main/java/org/example/ex03/connector/http/HttpProcessor.java
+++ src/main/java/org/example/ex03/connector/http/HttpProcessor.java
@@ -1,12 +1,22 @@
 package org.example.ex03.connector.http;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class HttpProcessor {
+
     public HttpProcessor(HttpConnector httpConnector) {
     }
 
     public void process(Socket socket) {
         SocketInputStream input=null;
+        OutputStream output=null;
+        try {
+            input=new SocketInputStream(socket.getInputStream(),2048);
+            output=socket.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/example/ex03/connector/http/SocketInputStream.java
+++ src/main/java/org/example/ex03/connector/http/SocketInputStream.java
@@ -1,4 +1,9 @@
 package org.example.ex03.connector.http;
 
+import java.io.InputStream;
+
 public class SocketInputStream {
+    public SocketInputStream(InputStream inputStream, int i) {
+
+    }
 }
